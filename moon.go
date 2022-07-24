61bde196f9e2e1d72a9ded7054dd5f3a9d0685af
荣方岩 <rfy1997@qq.com>
2022-07-24 23:13:45 +0800

    添加了基础的路由功能->使用Map实现

--- moon.go
+++ moon.go
@@ -5,21 +5,43 @@ import (
 	"net/http"
 )
 
-type Moon struct {
+// HandlerFunc 响应HTTP请求的Handler函数
+type HandlerFunc func(w http.ResponseWriter, r *http.Request)
+
+// Engine 实现 ServerHTTP 接口
+type Engine struct {
+	router map[string]HandlerFunc
 }
 
-func (m *Moon) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	if r.Method != "POST" {
-		switch r.URL.Path {
-		case "/":
-			_, err := fmt.Fprintf(w, "URL.Path = %q\n", r.URL.Path)
-			if err != nil {
-				return
-			}
-		}
+func (engine *Engine) addRoute(method string, path string, handler HandlerFunc) {
+	key := method + "-" + path
+	engine.router[key] = handler
+}
+
+func (engine *Engine) GET(path string, handler HandlerFunc) {
+	engine.addRoute("GET", path, handler)
+}
+
+func (engine *Engine) POST(path string, handler HandlerFunc) {
+	engine.addRoute("POST", path, handler)
+}
+
+func (engine *Engine) Run(addr string) (err error) {
+	return http.ListenAndServe(addr, engine)
+}
+
+func (engine *Engine) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	key := r.Method + "-" + r.URL.Path
+	if handler, ok := engine.router[key]; ok {
+		handler(w, r)
+	} else {
+		w.WriteHeader(http.StatusNotFound)
+		fmt.Fprintf(w, "404 NOT FOUND: %s\n", r.URL)
 	}
 }
 
-func New() *Moon {
-	return new(Moon)
+func New() *Engine {
+	engine := new(Engine)
+	engine.router = make(map[string]HandlerFunc)
+	return engine
 }
